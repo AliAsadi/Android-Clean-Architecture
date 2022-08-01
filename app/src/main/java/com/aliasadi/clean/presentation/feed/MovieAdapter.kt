36b2506ca10d2a49edd1f139058018563441628f
Ali Asadi <aliesaassadi@gmail.com>
2022-08-01 14:30:29 +0300

    update glide configuration

--- app/src/main/java/com/aliasadi/clean/presentation/feed/MovieAdapter.kt
+++ app/src/main/java/com/aliasadi/clean/presentation/feed/MovieAdapter.kt
@@ -1,13 +1,19 @@
 package com.aliasadi.clean.presentation.feed
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.aliasadi.clean.databinding.ItemMovieBinding
 import com.aliasadi.clean.domain.model.Movie
 import com.aliasadi.clean.presentation.feed.MovieAdapter.MovieViewHolder
 import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 
 /**
  * Created by Ali Asadi on 13/05/2020
@@ -36,11 +42,27 @@ class MovieAdapter(
         private val binding = ItemMovieBinding.bind(itemView)
 
         fun bind(movie: Movie) = with(binding) {
+            loadImage(image, movie.image)
             title.text = movie.title
             desc.text = movie.description
-            Glide.with(image).load(movie.image).into(image)
             root.setOnClickListener { onMovieClick(movie) }
         }
+
+        private fun loadImage(image: AppCompatImageView, url: String) = Glide.with(image)
+            .asDrawable()
+            .format(DecodeFormat.PREFER_RGB_565)
+            .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+            .thumbnail(getThumbnailRequest(image, url))
+            .sizeMultiplier(0.5F)
+            .load(url)
+            .into(image)
+
+        private fun getThumbnailRequest(imageView: ImageView, url: String): RequestBuilder<Drawable> = Glide.with(imageView)
+            .asDrawable()
+            .format(DecodeFormat.PREFER_RGB_565)
+            .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+            .sizeMultiplier(0.2F)
+            .load(url)
     }
 
 
