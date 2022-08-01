e8863067e8cbc000cc12fadfb6072f6c112cac7b
Ali Asadi <aliesaassadi@gmail.com>
2022-08-01 15:21:42 +0300

    add getResult() extension to Result class

--- app/src/main/java/com/aliasadi/clean/data/repository/movie/MovieRepositoryImpl.kt
+++ app/src/main/java/com/aliasadi/clean/data/repository/movie/MovieRepositoryImpl.kt
@@ -1,9 +1,9 @@
 package com.aliasadi.clean.data.repository.movie
 
-import android.util.Log
 import com.aliasadi.clean.domain.model.Movie
 import com.aliasadi.clean.domain.repository.MovieRepository
 import com.aliasadi.clean.domain.util.Result
+import com.aliasadi.clean.domain.util.getResult
 
 /**
  * Created by Ali Asadi on 13/05/2020
@@ -18,37 +18,28 @@ class MovieRepositoryImpl constructor(
 
     override suspend fun getMovie(movieId: Int): Result<Movie> = getMovieFromCache(movieId)
 
-    private suspend fun getMovieFromCache(movieId: Int): Result<Movie> {
-        return when (val result = cache.getMovie(movieId)) {
-            is Result.Success -> result
-            is Result.Error -> getMovieFromLocal(movieId)
-        }
-    }
+    private suspend fun getMovieFromCache(movieId: Int): Result<Movie> = cache.getMovie(movieId).getResult({
+        it
+    }, {
+        getMovieFromLocal(movieId)
+    })
 
     private suspend fun getMovieFromLocal(movieId: Int): Result<Movie> = local.getMovie(movieId)
 
-    private suspend fun getMoviesFromCache(): Result<List<Movie>> {
-        Log.d("XXX", "Getting movies from cache")
-        return when (val result = cache.getMovies()) {
-            is Result.Success -> result
-            is Result.Error -> getMoviesFromLocal()
-        }
-    }
+    private suspend fun getMoviesFromCache(): Result<List<Movie>> = cache.getMovies().getResult({
+        it
+    }, {
+        getMoviesFromLocal()
+    })
 
-    private suspend fun getMoviesFromLocal(): Result<List<Movie>> {
-        Log.d("XXX", "Getting movies from database")
-        return when (val result = local.getMovies()) {
-            is Result.Success -> {
-                refreshCache(result.data)
-                result
-            }
-
-            is Result.Error -> getMoviesFromRemote()
-        }
-    }
+    private suspend fun getMoviesFromLocal(): Result<List<Movie>> = local.getMovies().getResult({
+        refreshCache(it.data)
+        it
+    }, {
+        getMoviesFromRemote()
+    })
 
     private suspend fun getMoviesFromRemote(): Result<List<Movie>> {
-        Log.d("XXX", "Getting movies from remote")
         val result = remote.getMovies()
 
         if (result is Result.Success) {
--- app/src/main/java/com/aliasadi/clean/domain/util/Result.kt
+++ app/src/main/java/com/aliasadi/clean/domain/util/Result.kt
@@ -7,3 +7,6 @@ sealed class Result<T> {
     data class Success<T>(val data: T) : Result<T>()
     data class Error<T>(val error: Throwable) : Result<T>()
 }
+
+inline fun <T, R> Result<T>.getResult(success: (Result.Success<T>) -> R, error: (Result.Error<T>) -> R): R =
+    if (this is Result.Success) success(this) else error(this as Result.Error)
