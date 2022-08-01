e1cf754c0ca58f56def2faaa5e101878016a67a7
Ali Asadi <aliesaassadi@gmail.com>
2022-08-01 13:41:36 +0300

    formatting

--- app/src/main/java/com/aliasadi/clean/data/model/MovieData.kt
+++ app/src/main/java/com/aliasadi/clean/data/model/MovieData.kt
@@ -6,15 +6,8 @@ import com.google.gson.annotations.SerializedName
  * Created by Ali Asadi on 13/05/2020
  */
 data class MovieData(
-    @SerializedName("id")
-    val id: Int,
-
-    @SerializedName("description")
-    val description: String,
-
-    @SerializedName("image")
-    val image: String,
-
-    @SerializedName("title")
-    val title: String
+    @SerializedName("id") val id: Int,
+    @SerializedName("description") val description: String,
+    @SerializedName("image") val image: String,
+    @SerializedName("title") val title: String
 )
