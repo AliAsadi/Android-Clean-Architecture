a11bc728fd317fd5793509cce67da9e8231d270e
Ali Asadi <aliesaassadi@gmail.com>
2022-08-03 15:09:19 +0300

    update tests

--- app/src/test/java/com/aliasadi/clean/presentation/details/MovieDetailsViewModelTest.kt
+++ app/src/test/java/com/aliasadi/clean/presentation/details/MovieDetailsViewModelTest.kt
@@ -2,12 +2,16 @@ package com.aliasadi.clean.presentation.details
 
 import androidx.lifecycle.Observer
 import com.aliasadi.clean.domain.model.Movie
+import com.aliasadi.clean.domain.usecase.GetMovieDetailsUseCase
+import com.aliasadi.clean.domain.util.Result
 import com.aliasadi.clean.presentation.base.BaseViewModelTest
+import com.aliasadi.clean.presentation.util.rules.runBlockingTest
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
 
 /**
@@ -16,32 +20,37 @@ import org.mockito.junit.MockitoJUnitRunner
 @RunWith(MockitoJUnitRunner::class)
 class MovieDetailsViewModelTest : BaseViewModelTest() {
 
+    var movieId: Int = 1413
+
     @Mock
-    lateinit var movie: Movie
+    lateinit var getMovieDetailsUseCase: GetMovieDetailsUseCase
 
     private lateinit var viewModel: MovieDetailsViewModel
 
     @Before
     fun setUp() {
-        viewModel = MovieDetailsViewModel(movie, coroutineRule.testDispatcherProvider)
+        viewModel = MovieDetailsViewModel(movieId, getMovieDetailsUseCase, coroutineRule.testDispatcherProvider)
     }
 
     @Test
-    fun loadInitialState_movieAvailable_showMovieDetails() {
+    fun loadInitialState_movieAvailable_showMovieDetails() = coroutineRule.runBlockingTest {
+
         val movieObs: Observer<Movie> = mock()
+
+        `when`(getMovieDetailsUseCase.getMovie(movieId)).thenReturn(Result.Success(mock()))
 
         viewModel.getMovieLiveData().observeForever(movieObs)
 
         viewModel.loadInitialState()
 
-        Mockito.verify(movieObs).onChanged(movie)
+        Mockito.verify(movieObs).onChanged(any())
     }
 
     @Test
-    fun loadInitialState_movieNotAvailable_doNothing() {
-        viewModel = MovieDetailsViewModel(null, coroutineRule.testDispatcherProvider)
-
+    fun loadInitialState_movieNotAvailable_doNothing() = coroutineRule.runBlockingTest {
         val movieObs: Observer<Movie> = mock()
+
+        `when`(getMovieDetailsUseCase.getMovie(movieId)).thenReturn(Result.Error(mock()))
 
         viewModel.getMovieLiveData().observeForever(movieObs)
 
