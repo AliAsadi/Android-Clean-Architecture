491a13e2a548370e8f830b70b26ac472982bd6f8
Ali Asadi <aliesaassadi@gmail.com>
2022-08-19 11:59:25 +0300

    update

--- presentation/src/main/java/com/aliasadi/clean/main/MainViewModel.kt
+++ presentation/src/main/java/com/aliasadi/clean/main/MainViewModel.kt
@@ -1,10 +1,8 @@
 package com.aliasadi.clean.main
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.aliasadi.clean.base.BaseViewModel
-import com.aliasadi.clean.util.SingleLiveEvent
 import com.aliasadi.data.util.DispatchersProvider
 
 /**
@@ -13,23 +11,6 @@ import com.aliasadi.data.util.DispatchersProvider
 class MainViewModel(
     dispatchers: DispatchersProvider
 ) : BaseViewModel(dispatchers) {
-
-    private val navigationState: SingleLiveEvent<NavigationState> = SingleLiveEvent()
-
-    sealed class NavigationState {
-        object Feed : NavigationState()
-        object Favorite : NavigationState()
-    }
-
-    fun onFeedNavigationItemSelected() {
-        navigationState.value = NavigationState.Feed
-    }
-
-    fun onFavoriteNavigationItemSelected() {
-        navigationState.value = NavigationState.Favorite
-    }
-
-    fun getNavigationState(): LiveData<NavigationState> = navigationState
 
     class Factory(
         private val dispatchers: DispatchersProvider
