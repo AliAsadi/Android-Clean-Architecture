fbea8f4c1e4009afe5be91880b69e3c089737fdc
Ali Asadi <aliesaassadi@gmail.com>
2022-08-23 13:19:05 +0300

    update

---
+++ presentation/src/main/java/com/aliasadi/clean/moviedetails/MovieDetailsPlaceholderFragment.kt
@@ -0,0 +1,9 @@
+package com.aliasadi.clean.moviedetails
+
+import androidx.fragment.app.Fragment
+import com.aliasadi.clean.R
+
+/**
+ * @author by Ali Asadi on 23/08/2022
+ */
+class MovieDetailsPlaceholderFragment : Fragment(R.layout.fragment_movie_details_placeholder) 
