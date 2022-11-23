009e353c7c8c34d73a105a97e2722d2a60731635
kanake10 <10kanake@gmail.com>
2022-11-23 16:52:26 +0300

    introduce const vals in the API interface to remove hardcoded strings

--- data/src/main/java/com/aliasadi/data/api/MovieApi.kt
+++ data/src/main/java/com/aliasadi/data/api/MovieApi.kt
@@ -1,6 +1,6 @@
 package com.aliasadi.data.api
 
-import com.aliasadi.data.entities.MovieData
+import com.aliasadi.data.model.MovieDataDto
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -8,9 +8,13 @@ import retrofit2.http.Query
  * Created by Ali Asadi on 13/05/2020
  */
 interface MovieApi {
-    @GET("/movies")
-    suspend fun getMovies(): List<MovieData>
+    @GET(MOVIES_ENDPOINT)
+    suspend fun getMovies(): List<MovieDataDto>
 
-    @GET("/movies")
-    suspend fun search(@Query("q") query: String): List<MovieData>
+    @GET(MOVIES_ENDPOINT)
+    suspend fun search(@Query("q") query: String): List<MovieDataDto>
+
+    companion object{
+        const val MOVIES_ENDPOINT = "/movies"
+    }
 }
