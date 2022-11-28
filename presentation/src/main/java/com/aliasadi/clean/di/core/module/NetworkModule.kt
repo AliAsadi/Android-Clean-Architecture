57a708a46d309731e3f4f23c52869d46c0fac858
kanake10 <10kanake@gmail.com>
2022-11-29 01:03:06 +0300

    debug with okhttp

--- presentation/src/main/java/com/aliasadi/clean/di/core/module/NetworkModule.kt
+++ presentation/src/main/java/com/aliasadi/clean/di/core/module/NetworkModule.kt
@@ -1,15 +1,20 @@
 package com.aliasadi.clean.di.core.module
 
-import com.aliasadi.data.BuildConfig
+import com.aliasadi.data.BuildConfig.BASE_URL
 import com.aliasadi.data.api.MovieApi
-import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import java.util.concurrent.TimeUnit
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import com.preference.provider.PreferenceProvider.context
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
+
 
 /**
  * Created by Ali Asadi on 15/05/2020
@@ -20,17 +25,32 @@ class NetworkModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .addCallAdapterFactory(CoroutineCallAdapterFactory())
-            .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl(BuildConfig.BASE_URL)
-            .build()
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
     }
 
-    @Singleton
     @Provides
-    fun provideMovieApi(retrofit: Retrofit): MovieApi {
-        return retrofit.create(MovieApi::class.java)
+    @Singleton
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(ChuckerInterceptor(context!!))
+            .callTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+
+        return okHttpClient.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieApi(okHttpClient: OkHttpClient): MovieApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(MovieApi::class.java)
     }
 }
