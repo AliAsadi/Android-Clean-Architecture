b3f59bff967c7edf8327331553a158290b39793e
Ali Asadi <aliesaassadi@gmail.com>
2023-01-30 17:45:36 +0200

    add category to MovieListItem.kt

--- presentation/src/main/java/com/aliasadi/clean/entities/MovieListItem.kt
+++ presentation/src/main/java/com/aliasadi/clean/entities/MovieListItem.kt
@@ -7,6 +7,7 @@ sealed class MovieListItem {
     data class Movie(
         val id: Int,
         val imageUrl: String,
+        val category: String,
     ) : MovieListItem()
 
     data class Separator(val category: String) : MovieListItem()
