595db483673f79e773d0f3e3c1caeb9ccf540a67
Ali Asadi <aliesaassadi@gmail.com>
2023-01-30 23:57:34 +0200

    create MovieRemoteDataSource.kt

--- data/src/main/java/com/aliasadi/data/db/movies/MovieDatabase.kt
+++ data/src/main/java/com/aliasadi/data/db/movies/MovieDatabase.kt
@@ -3,15 +3,17 @@ package com.aliasadi.data.db.movies
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.aliasadi.data.entities.MovieDbData
+import com.aliasadi.data.entities.MovieRemoteKeyDbData
 
 /**
  * Created by Ali Asadi on 13/05/2020
  */
 @Database(
-    entities = [MovieDbData::class],
+    entities = [MovieDbData::class, MovieRemoteKeyDbData::class],
     version = 1,
     exportSchema = false
 )
 abstract class MovieDatabase : RoomDatabase() {
     abstract fun movieDao(): MovieDao
+    abstract fun movieRemoteKeysDao(): MovieRemoteKeyDao
 }
---
+++ data/src/main/java/com/aliasadi/data/db/movies/MovieRemoteKeyDao.kt
@@ -0,0 +1,23 @@
+package com.aliasadi.data.db.movies
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.aliasadi.data.entities.MovieRemoteKeyDbData
+
+/**
+ * @author by Ali Asadi on 30/01/2023
+ */
+@Dao
+interface MovieRemoteKeyDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveRemoteKeys(keys: List<MovieRemoteKeyDbData>)
+
+    @Query("SELECT * FROM movies_remote_keys WHERE id=:id")
+    suspend fun getRemoteKeyByMovieId(id: Int): MovieRemoteKeyDbData?
+
+    @Query("DELETE FROM movies_remote_keys")
+    suspend fun clearRemoteKeys()
+}
