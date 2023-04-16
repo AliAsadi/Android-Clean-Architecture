97a42dc3d007bda3f5d5bcfc369c2e684cccde92
Ali Asadi <aliesaassadi@gmail.com>
2023-04-16 22:38:23 +0300

    create theme and colors

---
+++ presentation/src/main/java/com/aliasadi/clean/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.aliasadi.clean.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black1A = Color(0xFF1A1A1A)
+val GrayB3 = Color(0xFFB4B3B3)
+val LightGrayD3 = Color(0xFFD6D6D6)
+val DarkGrayD3 = Color(0xFF787878)
---
+++ presentation/src/main/java/com/aliasadi/clean/ui/theme/Theme.kt
@@ -0,0 +1,51 @@
+package com.aliasadi.clean.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+/**
+ * @author by Ali Asadi on 16/04/2023
+ */
+private val DarkColors = darkColorScheme(
+    primary = Black1A,
+    primaryContainer = DarkGrayD3,
+)
+
+private val LightColors = lightColorScheme(
+    primary = Color.White,
+    primaryContainer = DarkGrayD3,
+)
+
+@Composable
+fun AppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+
+    // val dynamicColor = darkTheme && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    // val colors = when {
+    //     dynamicColor && darkTheme -> {
+    //         dynamicDarkColorScheme(LocalContext.current)
+    //     }
+    //     dynamicColor && !darkTheme -> {
+    //         dynamicLightColorScheme(LocalContext.current)
+    //     }
+    //     darkTheme -> DarkColors
+    //     else -> LightColors
+    // }
+
+    val colors = if (darkTheme) {
+        DarkColors
+    } else {
+        LightColors
+    }
+
+    MaterialTheme(
+        colorScheme = colors,
+        content = content
+    )
+}
