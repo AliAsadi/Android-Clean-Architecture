09a883e8e0995c9c4e178a64f2c3023337c63d30
Ali Asadi <aliesaassadi@gmail.com>
2023-04-18 18:59:29 +0300

    update MovieDetailsFragment to use compose only

--- presentation/src/main/java/com/aliasadi/clean/ui/moviedetails/MovieDetailsFragment.kt
+++ presentation/src/main/java/com/aliasadi/clean/ui/moviedetails/MovieDetailsFragment.kt
@@ -1,17 +1,9 @@
 package com.aliasadi.clean.ui.moviedetails
 
-import android.graphics.drawable.Drawable
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import androidx.core.content.ContextCompat
+import androidx.compose.runtime.Composable
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
-import com.aliasadi.clean.R
-import com.aliasadi.clean.databinding.FragmentMovieDetailsBinding
-import com.aliasadi.clean.ui.base.BaseFragment
-import com.aliasadi.clean.util.launchAndRepeatWithViewLifecycle
-import com.bumptech.glide.Glide
+import com.aliasadi.clean.ui.base.BaseComposeFragment
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -20,7 +12,7 @@ import javax.inject.Inject
  */
 
 @AndroidEntryPoint
-class MovieDetailsFragment : BaseFragment<FragmentMovieDetailsBinding>() {
+class MovieDetailsFragment : BaseComposeFragment() {
 
     private val args: MovieDetailsFragmentArgs by navArgs()
 
@@ -31,43 +23,10 @@ class MovieDetailsFragment : BaseFragment<FragmentMovieDetailsBinding>() {
         MovieDetailsViewModel.provideFactory(factory, args.movieId)
     }
 
-    override fun inflateViewBinding(inflater: LayoutInflater): FragmentMovieDetailsBinding =
-        FragmentMovieDetailsBinding.inflate(inflater)
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        setupListeners()
-        observeViewModel()
-    }
-
-    private fun setupListeners() = with(binding) {
-        favorite.setOnClickListener {
+    @Composable
+    override fun Content() {
+        MovieDetailsRoute(viewModel) {
             viewModel.onFavoriteClicked()
         }
     }
-
-    private fun observeViewModel() = with(viewModel) {
-        launchAndRepeatWithViewLifecycle {
-            uiState.collect { handleMovieDetailsUiState(it) }
-        }
-    }
-
-    private fun handleMovieDetailsUiState(movieState: MovieDetailsViewModel.MovieDetailsUiState) {
-        binding.movieTitle.text = movieState.title
-        binding.description.text = movieState.description
-        loadImage(movieState.imageUrl)
-        updateFavoriteDrawable(getFavoriteDrawable(movieState.isFavorite))
-    }
-
-    private fun getFavoriteDrawable(favorite: Boolean): Drawable? = if (favorite) {
-        ContextCompat.getDrawable(requireContext(), R.drawable.ic_favorite_fill_white_48)
-    } else {
-        ContextCompat.getDrawable(requireContext(), R.drawable.ic_favorite_border_white_48)
-    }
-
-    private fun updateFavoriteDrawable(drawable: Drawable?) = with(binding.favorite) {
-        setImageDrawable(drawable)
-    }
-
-    private fun loadImage(url: String) =
-        Glide.with(this).load(url).placeholder(R.color.light_gray).error(R.drawable.bg_image).into(binding.image)
 }
