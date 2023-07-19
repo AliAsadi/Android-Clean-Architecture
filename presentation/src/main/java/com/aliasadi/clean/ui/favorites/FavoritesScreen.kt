4bae49c0a8661472962a0a7c44406d34c344dfa7
AliAsadi <aliesaassadi@gmail.com>
2023-07-19 10:39:25 +0300

    create FavoritesScreen

--- presentation/src/main/java/com/aliasadi/clean/ui/favorites/FavoritesScreen.kt
+++ presentation/src/main/java/com/aliasadi/clean/ui/favorites/FavoritesScreen.kt
@@ -1,20 +1,46 @@
 package com.aliasadi.clean.ui.favorites
 
+
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.paging.PagingData
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.aliasadi.clean.R
+import com.aliasadi.clean.entities.MovieListItem
 import com.aliasadi.clean.ui.widget.EmptyStateView
+import com.aliasadi.clean.ui.widget.Loader
+import com.aliasadi.clean.ui.widget.MovieList
 import com.aliasadi.clean.util.preview.PreviewContainer
-
+import kotlinx.coroutines.flow.flowOf
 
 @Composable
-fun FavoritesScreen() {
+fun FavoritesScreen(
+    favoriteUiState: FavoritesViewModel.FavoriteUiState,
+    movies: LazyPagingItems<MovieListItem>,
+    onMovieClick: (movieId: Int) -> Unit
+) {
     Surface {
-        EmptyStateView(
-            titleRes = R.string.no_favorite_movies_at_the_moment
-        )
+        val isLoading = favoriteUiState.isLoading
+        val noDataAvailable = favoriteUiState.noDataAvailable
+
+        MovieList(movies = movies, onMovieClick = onMovieClick)
+
+        if (isLoading) {
+            Box(contentAlignment = Alignment.Center) {
+                Loader()
+            }
+        } else {
+            if (noDataAvailable) {
+                EmptyStateView(titleRes = R.string.no_favorite_movies_at_the_moment)
+            }
+        }
+
+
     }
 }
 
@@ -23,7 +49,18 @@ fun FavoritesScreen() {
 @Preview(showSystemUi = true, name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun FavoritesScreenPreview() {
+    val movieItems: List<MovieListItem> = listOf(
+        MovieListItem.Separator("Action"),
+        MovieListItem.Movie(1, "image1.jpg", "Action"),
+        MovieListItem.Movie(2, "image2.jpg", "Comedy"),
+        MovieListItem.Separator("Drama"),
+        MovieListItem.Movie(3, "image3.jpg", "Drama")
+    )
+
     PreviewContainer {
-        FavoritesScreen()
+        val movies = flowOf(PagingData.from(movieItems)).collectAsLazyPagingItems()
+        FavoritesScreen(FavoritesViewModel.FavoriteUiState(), movies) {
+
+        }
     }
 }
