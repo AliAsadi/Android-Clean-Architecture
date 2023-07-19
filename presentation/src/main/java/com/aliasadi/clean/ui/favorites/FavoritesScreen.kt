63a422c88d774361815ba390e6223ac9102b09bd
AliAsadi <aliesaassadi@gmail.com>
2023-07-19 18:15:20 +0300

    create FavoritesScreen

--- presentation/src/main/java/com/aliasadi/clean/ui/favorites/FavoritesScreen.kt
+++ presentation/src/main/java/com/aliasadi/clean/ui/favorites/FavoritesScreen.kt
@@ -5,18 +5,53 @@ import android.content.res.Configuration
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.paging.CombinedLoadStates
 import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.aliasadi.clean.R
 import com.aliasadi.clean.entities.MovieListItem
+import com.aliasadi.clean.ui.favorites.FavoritesViewModel.NavigationState.MovieDetails
+import com.aliasadi.clean.ui.moviedetails.MovieDetailsActivity
 import com.aliasadi.clean.ui.widget.EmptyStateView
 import com.aliasadi.clean.ui.widget.Loader
 import com.aliasadi.clean.ui.widget.MovieList
 import com.aliasadi.clean.util.preview.PreviewContainer
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+@Composable
+fun FavoritesPage(
+    viewModel: FavoritesViewModel,
+    loadStateListener: (CombinedLoadStates, Int) -> Unit,
+    onMovieClick: (movieId: Int) -> Unit
+) {
+    val uiState by viewModel.uiState.collectAsState()
+    val movies = viewModel.movies.collectAsLazyPagingItems()
+    loadStateListener(movies.loadState, movies.itemCount)
+    val context = LocalContext.current
+
+    LaunchedEffect(key1 = Unit) {
+        viewModel.navigationState.onEach {
+            when (it) {
+                is MovieDetails -> MovieDetailsActivity.start(context, it.movieId)
+            }
+        }.launchIn(this)
+    }
+
+    FavoritesScreen(
+        favoriteUiState = uiState,
+        movies = movies,
+        onMovieClick = onMovieClick
+    )
+}
 
 @Composable
 fun FavoritesScreen(
@@ -39,8 +74,6 @@ fun FavoritesScreen(
                 EmptyStateView(titleRes = R.string.no_favorite_movies_at_the_moment)
             }
         }
-
-
     }
 }
 
