10c454d689cc586df317e04fe9df8b2013cf51a3
AliAsadi <aliesaassadi@gmail.com>
2023-07-21 21:25:21 +0300

    create BottomNavigationView

---
+++ presentation/src/main/java/com/aliasadi/clean/ui/widget/BottomNavigationView.kt
@@ -0,0 +1,40 @@
+package com.aliasadi.clean.ui.widget
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+data class BottomNavigationItem(
+    val tabName: String,
+    val imageVector: ImageVector,
+    val route: String,
+)
+
+@Composable
+fun BottomNavigationView(
+    items: List<BottomNavigationItem>,
+    navController: NavController,
+    onItemClick: (BottomNavigationItem) -> Unit
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    NavigationBar {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            NavigationBarItem(
+                selected = selected,
+                onClick = { onItemClick(item) },
+                icon = {
+                    Icon(imageVector = item.imageVector, contentDescription = null)
+                },
+                label = {
+                    Text(text = item.tabName)
+                }
+            )
+        }
+    }
+}
