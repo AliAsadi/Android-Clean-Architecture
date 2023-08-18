de41493aef5cd6e1d901c63b0a3906aba04cc04c
AliAsadi <aliesaassadi@gmail.com>
2023-08-18 20:40:53 +0300

    update padding

--- presentation/src/main/java/com/aliasadi/clean/ui/widget/MovieList.kt
+++ presentation/src/main/java/com/aliasadi/clean/ui/widget/MovieList.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -25,6 +26,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -82,9 +85,9 @@ private fun MovieItem(
 private fun Separator(title: String) {
     Text(
         text = title,
-        style = MaterialTheme.typography.titleLarge,
+        style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Medium),
         modifier = Modifier
-            .padding(8.dp)
+            .padding(start = 8.dp, top = 3.dp, bottom = 3.dp)
             .height(48.dp)
             .fillMaxWidth()
             .wrapContentSize(Alignment.CenterStart)
