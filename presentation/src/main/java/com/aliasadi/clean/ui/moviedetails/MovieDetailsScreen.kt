3296788483d2dee93cf2efc9da630aab3aa346f8
AliAsadi <aliesaassadi@gmail.com>
2023-08-18 23:54:03 +0300

    optimize imports

--- presentation/src/main/java/com/aliasadi/clean/ui/moviedetails/MovieDetailsScreen.kt
+++ presentation/src/main/java/com/aliasadi/clean/ui/moviedetails/MovieDetailsScreen.kt
@@ -28,7 +28,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import coil.compose.AsyncImage
 import coil.compose.SubcomposeAsyncImage
 import com.aliasadi.clean.R
 import com.aliasadi.clean.R.drawable
