1469e2118eff5fd2e40f0f886fac092a7654831f
AliAsadi <aliesaassadi@gmail.com>
2023-12-11 02:06:24 +0200

    update header padding

--- presentation/src/main/java/com/aliasadi/clean/ui/widget/MovieList.kt
+++ presentation/src/main/java/com/aliasadi/clean/ui/widget/MovieList.kt
@@ -7,10 +7,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -19,7 +17,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -90,10 +87,8 @@ private fun Separator(title: String) {
         text = title,
         style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Medium),
         modifier = Modifier
-            .padding(start = 8.dp, top = 3.dp, bottom = 3.dp)
-            .height(48.dp)
+            .padding(10.dp)
             .fillMaxWidth()
-            .wrapContentSize(Alignment.CenterStart)
     )
 }
 
