456a6ff84f137c4bc9e7e5a090e2e63f109e63f8
AliAsadi <aliesaassadi@gmail.com>
2024-05-05 13:41:35 +0300

    remove unused method

--- presentation/src/main/java/com/aliasadi/clean/ui/search/SearchViewModel.kt
+++ presentation/src/main/java/com/aliasadi/clean/ui/search/SearchViewModel.kt
@@ -62,8 +62,6 @@ class SearchViewModel @Inject constructor(
     fun onMovieClicked(movieId: Int) =
         _navigationState.tryEmit(NavigationState.MovieDetails(movieId))
 
-    fun getSearchQuery(): CharSequence? = savedStateHandle.get<String>(KEY_SEARCH_QUERY)
-
     fun onLoadStateUpdate(loadState: CombinedLoadStates, itemCount: Int) {
         val showLoading = loadState.refresh is LoadState.Loading
         val showNoData = loadState.append.endOfPaginationReached && itemCount < 1
