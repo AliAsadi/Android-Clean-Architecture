61cc913a0f986da00c49cf59f1bebd097d4c6372
AliAsadi <aliesaassadi@gmail.com>
2024-05-24 21:45:49 +0300

    fix detekt

--- presentation/src/main/kotlin/com/aliasadi/clean/ui/moviedetails/MovieDetailsScreen.kt
+++ presentation/src/main/kotlin/com/aliasadi/clean/ui/moviedetails/MovieDetailsScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
