bd91627e87409b286d88fecee0c8dcf7d7cf1781
AliAsadi <aliesaassadi@gmail.com>
2024-07-05 20:53:08 +0300

    rename BaseViewModelTest to BaseTest

--- presentation/src/test/kotlin/com/aliasadi/clean/presentation/base/BaseTest.kt
+++ presentation/src/test/kotlin/com/aliasadi/clean/presentation/base/BaseTest.kt
@@ -3,14 +3,13 @@ package com.aliasadi.clean.presentation.base
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.aliasadi.clean.presentation.util.rules.CoroutineTestRule
 import com.aliasadi.clean.presentation.util.rules.runTest
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestScope
 import org.junit.Rule
 
 /**
  * Created by Ali Asadi on 16/05/2020
  **/
-open class BaseViewModelTest {
+open class BaseTest {
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
@@ -18,6 +17,5 @@ open class BaseViewModelTest {
     @get:Rule
     var coroutineRule = CoroutineTestRule()
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     fun runTest(block: suspend TestScope.() -> Unit) = coroutineRule.runTest(block)
 }
