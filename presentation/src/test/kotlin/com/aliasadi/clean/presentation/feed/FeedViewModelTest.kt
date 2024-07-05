320ded51b4e81406e9e1b50e2af39a24c40f6413
AliAsadi <aliesaassadi@gmail.com>
2024-07-05 21:06:53 +0300

    update

--- presentation/src/test/kotlin/com/aliasadi/clean/presentation/feed/FeedViewModelTest.kt
+++ presentation/src/test/kotlin/com/aliasadi/clean/presentation/feed/FeedViewModelTest.kt
@@ -6,15 +6,17 @@ import androidx.paging.LoadStates
 import androidx.paging.PagingData
 import app.cash.turbine.test
 import com.aliasadi.clean.entities.MovieListItem
-import com.aliasadi.clean.presentation.base.BaseViewModelTest
+import com.aliasadi.clean.presentation.base.BaseTest
+import com.aliasadi.clean.ui.feed.FeedNavigationState
+import com.aliasadi.clean.ui.feed.FeedUiState
 import com.aliasadi.clean.ui.feed.FeedViewModel
 import com.aliasadi.clean.ui.feed.usecase.GetMoviesWithSeparators
 import com.aliasadi.clean.util.NetworkMonitor
 import com.google.common.truth.Truth.assertThat
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceUntilIdle
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -26,15 +28,11 @@ import org.mockito.junit.MockitoJUnitRunner
  * Created by Ali Asadi on 16/05/2020
  **/
 
-@OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(MockitoJUnitRunner::class)
-class FeedViewModelTest : BaseViewModelTest() {
+class FeedViewModelTest : BaseTest() {
 
-    @Mock
-    lateinit var getMoviesWithSeparators: GetMoviesWithSeparators
-
-    @Mock
-    lateinit var networkMonitor: NetworkMonitor
+    private val getMoviesWithSeparators: GetMoviesWithSeparators = mock()
+    private val networkMonitor: NetworkMonitor = mock()
 
     private lateinit var viewModel: FeedViewModel
 
@@ -45,6 +43,8 @@ class FeedViewModelTest : BaseViewModelTest() {
     @Before
     fun setUp() {
         `when`(getMoviesWithSeparators.movies(pageSize = anyInt())).thenReturn(pagingData)
+        `when`(networkMonitor.getInitialState()).thenReturn(NetworkMonitor.NetworkState.Available)
+        `when`(networkMonitor.networkState).thenReturn(flowOf(NetworkMonitor.NetworkState.Available))
         viewModel = FeedViewModel(
             getMoviesWithSeparators = getMoviesWithSeparators,
             networkMonitor = networkMonitor,
@@ -53,55 +53,53 @@ class FeedViewModelTest : BaseViewModelTest() {
     }
 
     @Test
-    fun onLoadStateUpdate_onLoading_showLoadingView() = runTest {
-        viewModel.onLoadStateUpdate(getLoadState(LoadState.Loading))
-
+    fun `test showing loader when loading data`() = runTest {
+        viewModel.onLoadStateUpdate(mockLoadState(LoadState.Loading))
         assertThat(viewModel.uiState.value.showLoading).isTrue()
     }
 
     @Test
-    fun onLoadStateUpdate_onFailure_hideLoadingAndShowErrorMessage() = runTest {
+    fun `test showing error message on loading failure`() = runTest {
         val errorMessage = "error"
-        viewModel.onLoadStateUpdate(getLoadState(LoadState.Error(Throwable(errorMessage))))
+        viewModel.onLoadStateUpdate(mockLoadState(LoadState.Error(Throwable(errorMessage))))
 
         viewModel.uiState.test {
-            val emission: FeedViewModel.FeedUiState = awaitItem()
+            val emission: FeedUiState = awaitItem()
             assertThat(emission.showLoading).isFalse()
             assertThat(emission.errorMessage).isEqualTo(errorMessage)
         }
     }
 
     @Test
-    fun onLoadStateUpdate_onSuccess_hideLoadingAndShowMovies() = runTest {
-        viewModel.onLoadStateUpdate(getLoadState(LoadState.NotLoading(true)))
+    fun `test showing movies on loading success`() = runTest {
+        viewModel.onLoadStateUpdate(mockLoadState(LoadState.NotLoading(true)))
 
         // TODO - test movies flow
         viewModel.uiState.test {
-            val emission: FeedViewModel.FeedUiState = awaitItem()
+            val emission: FeedUiState = awaitItem()
             assertThat(emission.showLoading).isFalse()
             assertThat(emission.errorMessage).isNull()
         }
     }
 
     @Test
-    fun onMovieClicked_navigateToMovieDetails() = runTest {
+    fun `verify navigation to movie details when a movie is clicked`() = runTest {
         val movieId = 1
 
         launch {
             viewModel.navigationState.test {
                 val emission = awaitItem()
-                assertThat(emission).isInstanceOf(FeedViewModel.NavigationState.MovieDetails::class.java)
-
+                assertThat(emission).isInstanceOf(FeedNavigationState.MovieDetails::class.java)
                 when (emission) {
-                    is FeedViewModel.NavigationState.MovieDetails -> assertThat(emission.movieId).isEqualTo(movieId)
+                    is FeedNavigationState.MovieDetails -> assertThat(emission.movieId).isEqualTo(movieId)
                 }
             }
         }
 
         viewModel.onMovieClicked(movieId)
-
     }
 
-    private fun getLoadState(state: LoadState): CombinedLoadStates =
-        CombinedLoadStates(state, state, state, LoadStates(state, state, state))
+    private fun mockLoadState(state: LoadState): CombinedLoadStates = CombinedLoadStates(
+        state, state, state, LoadStates(state, state, state)
+    )
 }
