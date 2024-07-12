b985e482f30f2b50112e9e4df3705b3a5f071599
AliAsadi <aliesaassadi@gmail.com>
2024-07-12 13:38:21 +0300

    create test-core module

---
+++ test-core/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    flavorDimensions += "environment"
+
+    productFlavors {
+        create("prod") {
+            isDefault = true
+        }
+        create("mock")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    namespace = "com.aliasadi.coretest"
+}
+
+dependencies {
+
+}
