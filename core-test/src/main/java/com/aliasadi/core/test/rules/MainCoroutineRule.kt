7c8ff8a43a1fa3ed29e74127c386a83060298a14
AliAsadi <aliesaassadi@gmail.com>
2024-07-13 20:45:27 +0300

    fix detekt

--- core-test/src/main/java/com/aliasadi/core/test/rules/MainCoroutineRule.kt
+++ core-test/src/main/java/com/aliasadi/core/test/rules/MainCoroutineRule.kt
@@ -3,7 +3,6 @@ package com.aliasadi.core.test.rules
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestDispatcher
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
