7ff0a3850a84a2f4e07d8b3cc2598983d8302fe9
AliAsadi <aliesaassadi@gmail.com>
2024-08-05 11:37:05 +0300

    convert gradle to kotlin dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,15 +6,15 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath libs.gradle
-        classpath libs.kotlin.gradle.plugin
+        classpath(libs.gradle)
+        classpath(libs.kotlin.gradle.plugin)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
 plugins {
-    id("io.gitlab.arturbosch.detekt") version "$detekt_version" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.22.0" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
     id("org.jetbrains.kotlinx.kover") version "0.8.2" apply false
@@ -24,18 +24,17 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven { url 'https://jitpack.io' }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
-
 subprojects {
-    //Apply Detekt plugin for all modules (presentation, data, domain)
-    apply from: "$rootDir/detekt.gradle"
-    //Apply Kover plugin for all modules
-    apply from: "$rootDir/kover.gradle"
+    // Apply Detekt plugin for all modules (presentation, data, domain)
+    apply(from = "$rootDir/detekt.gradle")
+    // Apply Kover plugin for all modules
+    apply(from = "$rootDir/kover.gradle")
 }
 
-tasks.register('clean', Delete) {
-    delete rootProject.buildDir
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
