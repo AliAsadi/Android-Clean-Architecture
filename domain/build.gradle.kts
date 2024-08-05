92b2dee4e8bbcb2251f488fc1f6f546e8b2a07f4
AliAsadi <aliesaassadi@gmail.com>
2024-08-05 10:03:40 +0300

    convert groovy gradle to Kotlin DSL

---
+++ domain/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    flavorDimensions.add("environment")
+
+    productFlavors {
+        create("prod") {
+            isDefault = true
+        }
+        create("mock")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    namespace = "com.aliasadi.domain"
+}
+
+dependencies {
+    implementation("androidx.paging:paging-common-ktx:3.3.0")
+}
