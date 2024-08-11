eef9c44b34c41e9b3ca98c0070e7f2333086eae4
AliAsadi <aliesaassadi@gmail.com>
2024-08-11 22:11:40 +0300

    remove buildScript block, use plugins instead

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath(libs.gradle)
-        classpath(libs.kotlin.gradle.plugin)
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
-}
-
 plugins {
+    alias(libs.plugins.android) apply false
+    alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
