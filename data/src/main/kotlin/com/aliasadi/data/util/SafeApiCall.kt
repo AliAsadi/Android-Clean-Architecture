0420187586d2697531434a3b950bbc84df29d66d
Shubham Agarwal <mwshubham@gmail.com>
2025-09-01 02:41:34 +0530

    Refactor: Replace safeApiCall with runCatching
    The `safeApiCall` utility function has been removed.
    All usages of `safeApiCall` in `MovieRemoteDataSource` have been replaced with the standard Kotlin `runCatching` function.

--- data/src/main/kotlin/com/aliasadi/data/util/SafeApiCall.kt
+++
@@ -1,8 +0,0 @@
-package com.aliasadi.data.util
-
-
-suspend fun <T> safeApiCall(apiCall: suspend () -> T): Result<T> = try {
-    Result.success(apiCall.invoke())
-} catch (e: Exception) {
-    Result.failure(e)
-}
--- data/src/prod/kotlin/com/aliasadi/data/repository/movie/MovieRemoteDataSource.kt
+++ data/src/prod/kotlin/com/aliasadi/data/repository/movie/MovieRemoteDataSource.kt
@@ -2,7 +2,6 @@ package com.aliasadi.data.repository.movie
 
 import com.aliasadi.data.api.MovieApi
 import com.aliasadi.data.entities.MovieData
-import com.aliasadi.data.util.safeApiCall
 
 /**
  * Created by Ali Asadi on 13/05/2020
@@ -11,19 +10,19 @@ class MovieRemoteDataSource(
     private val movieApi: MovieApi
 ) : MovieDataSource.Remote {
 
-    override suspend fun getMovies(page: Int, limit: Int): Result<List<MovieData>> = safeApiCall {
+    override suspend fun getMovies(page: Int, limit: Int): Result<List<MovieData>> = runCatching {
         movieApi.getMovies(page, limit)
     }
 
-    override suspend fun getMovies(movieIds: List<Int>): Result<List<MovieData>> = safeApiCall {
+    override suspend fun getMovies(movieIds: List<Int>): Result<List<MovieData>> = runCatching {
         movieApi.getMovies(movieIds)
     }
 
-    override suspend fun getMovie(movieId: Int): Result<MovieData> = safeApiCall {
+    override suspend fun getMovie(movieId: Int): Result<MovieData> = runCatching {
         movieApi.getMovie(movieId)
     }
 
-    override suspend fun search(query: String, page: Int, limit: Int): Result<List<MovieData>> = safeApiCall {
+    override suspend fun search(query: String, page: Int, limit: Int): Result<List<MovieData>> = runCatching {
         movieApi.search(query, page, limit)
     }
 }
